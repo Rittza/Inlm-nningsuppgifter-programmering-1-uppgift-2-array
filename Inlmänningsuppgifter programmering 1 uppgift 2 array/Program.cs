827261dbf832c19b525ada922ebebae759aca0e2
Meysam03 <alre03001@utb.vaxjo.se>
2023-01-20 10:42:11 +0100

    Add project files.

---
+++ Inlmänningsuppgifter programmering 1 uppgift 2 array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlämnningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            int[] heltal = new int[antal];
+            for(int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("Skriv in ett heltal");
+                heltal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Här är hetalen du skrev in");
+            for(int i = 0; i < antal; i++)
+            {
+                Console.WriteLine(heltal[i]);
+            }
+        }
+    }
+}
